7b2d06535322a54d35f36158f15164b14b35d173
Hugh O'Reilly <horeilly1101@gmail.com>
2019-03-16 00:11:26 -0500

    derivatives appear on webpage

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
-function loadDoc() {
+function loadDerivative() {
 	var request = new XMLHttpRequest();
 	request.open('GET', 'http://localhost:4567/differentiate/x/x', true);
 	request.onload = function () {
 		console.log(this.response)
+		var header = document.getElementById("header2")
+		header.innerHTML = this.response
 	}
 
 	request.send();
