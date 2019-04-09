d3b04ac3f7e75b454412d9815cfac7d6c635f2c7
Hugh O'Reilly <horeilly1101@gmail.com>
2019-04-08 23:58:10 -0500

    added comments

--- scripts.js
+++ scripts.js
@@ -1,29 +1,45 @@
+/**
+ * Query the back end to get the derivative of a given expression.
+ * Retrieves this expression from a textfield in the html.
+ * Installs the derivative in the html.
+ */
 function loadDerivative() {
+	// create request and get inputs
 	var request = new XMLHttpRequest()
 	var expr = document.getElementById("exprInput").value
 	var variable = document.getElementById("varInput").value
 
+	// create url
 	var url = 'http://localhost:4567/differentiate/' + cleanInput(expr) + '/' + cleanInput(variable)
 
+	// describe the request
 	request.open('GET', url, true)
 
 	request.onload = function () {
 		var header = document.getElementById("header")
 		
 		if (request.status != 400) {
-			console.log(this.response)
+			// insert the derivative into the web page
 			var obj = JSON.parse(this.response)
 			header.innerHTML = "$$\\frac{\\partial}{\\partial " + obj.data.var + "}\\Big[" + obj.data.expression + "\\Big] = " + obj.data.result + "$$"
 			MathJax.Hub.Queue(["Typeset",MathJax.Hub, header])
 		} else {
+			// insert an error message into the web page
 			header.innerHTML = "$invalid$ $inputs$"
 			MathJax.Hub.Queue(["Typeset",MathJax.Hub, header])
 		}
 	}
 
+	// send the request
 	request.send()
 }
 
+/**
+ * Replace all 'unsafe' url characters with their escaped equivalents.
+ * (e.g. replace '/' with '%2F')
+ * @param {String} input string to be cleaned
+ * @return cleaned string
+ */
 function cleanInput(input) {
 	newStr = ""
 	for (char of input) {
